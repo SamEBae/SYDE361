a17228688b54c020e94872665ee4f27df8a5194c
Sammie Bae <sammie@goshippo.com>
2017-06-18 18:08:52 -0400

    Adds basic note detection functionality

--- processing/processing.ino
+++ processing/processing.ino
@@ -4,20 +4,29 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
+#include "pitch_utils.h"
+#include "note_name.h"
+#include "tuned_note.h"
+
 // GUItool: begin automatically generated code
 AudioInputUSB            usb1;           //xy=71,90
 AudioFilterBiquad        biquad1;        //xy=229,169
 AudioOutputUSB           usb2;           //xy=448,94
 AudioOutputI2S           i2s1;           //xy=453,164
-AudioAnalyzeNoteFrequency note1;
+AudioAnalyzeNoteFrequency notefreq;
 AudioConnection          patchCord1(usb1, 0, i2s1, 0);
 AudioConnection          patchCord2(usb1, 0, usb2, 0);
 AudioConnection          patchCord3(usb1, 1, biquad1, 0);
 AudioConnection          patchCord4(biquad1, 0, usb2, 1);
 AudioConnection          patchCord5(biquad1, 0, i2s1, 1);
-AudioConnection          patchCord6(usb1, 0, note1 ,0);
+AudioConnection          patchCord6(usb1, 0, notefreq ,0);
 AudioControlSGTL5000     sgtl5000_1;     //xy=219,29
 // GUItool: end automatically generated code
+
+// Global access
+std::array<float, 88> pitch_freqs;
+std::array<note_name *, 88> pitch_names;
+//
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,16 +34,32 @@ void setup() {
   AudioMemory(30);
   delay(250);
 
-  note1.begin(0.15);
+  notefreq.begin(0.15);
   sgtl5000_1.enable();
   sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN);
   sgtl5000_1.volume(0.5);
-  biquad1.setLowpass(0,500, 0.707);    
+  biquad1.setLowpass(0,500, 0.707);
+
+  // Pitch detection:
+  float reference = 440.0;
+  pitch_freqs = get_pitch_freqs(reference);
+  pitch_names = get_pitch_names();
 }
 
 void loop() {
   delay(20);
-  if (note1.available()) {
-    Serial.println(note1.read());
+  if (notefreq.available()) {
+    int freq = notefreq.read();
+    float prob = notefreq.probability();
+    
+    tuned_note n = freq_to_note(freq, pitch_freqs);
+    int index = n.getPitch();
+    double distance = n.getDistance();
+
+    note_name note = *pitch_names[index];
+    
+    Serial.println(freq);
+    Serial.println(prob);
+    Serial.printf("closest tuned_note %c%c\n", note.getName(), note.getModifier());
   }
 }
